d3ebaa53c15c5c6f16bdbd027e2b8ea0eab06107
hit0tian <128696895+hit0tian@users.noreply.github.com>
2024-11-08 00:25:33 +0800

    Add files via upload

---
+++ solution/src/test/java/org/example/AppTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testMultiply() {
+        Solution solution = new Solution();
+        String a = solution.multiply("2","3");
+        assertEquals("6",a);
+        a = solution.multiply("123","456");
+        assertEquals("56088",a);
+
+    }
+
+//测试加法，无进位和有进位是否正常
+// 例1："2"+"3"="5"
+// 例2："9"+"2"="11"
+
+    @Test
+    public void addStrings() {
+        Solution solution = new Solution();
+        String ans = solution.addStrings("2","3");
+        assertEquals("5",ans);
+        ans = solution.addStrings("9","2");
+        assertEquals("11",ans);
+    }
+
+}
